fd3f810dd2eab380fdfb5f6beb07b87bc4fc2ce2
Nikita <44435714+Nickpicnic@users.noreply.github.com>
2019-10-20 17:02:45 +0300

    Add files via upload

---
+++ React/7_ReactStateDiceExercise/build/precache-manifest.fc792d30b0337e93cb6005b1547971e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c19790271b810a0856ee9c8665ab4ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc0357b4bf713e3ced35",
+    "url": "/static/css/main.73fc0cf1.chunk.css"
+  },
+  {
+    "revision": "2f4870a4e1f0694bb531",
+    "url": "/static/js/2.1d49545f.chunk.js"
+  },
+  {
+    "revision": "dc0357b4bf713e3ced35",
+    "url": "/static/js/main.876955bd.chunk.js"
+  },
+  {
+    "revision": "b41d8a00c6389595aabc",
+    "url": "/static/js/runtime-main.b64d33b0.js"
+  }
+]);
