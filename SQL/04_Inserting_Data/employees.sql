fa7fd30d5a8dc9974798976c709416341ed6e910
Nikita <44435714+Nickpicnic@users.noreply.github.com>
2019-10-27 23:05:50 +0300

    Update employees.sql

--- SQL/04_Inserting_Data/employees.sql
+++ SQL/04_Inserting_Data/employees.sql
@@ -7,3 +7,14 @@ CREATE TABLE employees(
     current_status VARCHAR(255) NOT NULL DEFAULT 'employed', 
     PRIMARY KEY(id)
 );
+
+-- another way:
+
+CREATE TABLE employees2 (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    age INT NOT NULL,
+    current_status VARCHAR(255) NOT NULL DEFAULT 'employed'
+);
